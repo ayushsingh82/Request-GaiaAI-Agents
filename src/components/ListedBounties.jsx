2e8f1568827576f83c6ece3e0e91f2c3c292f47a
OmkarK97 <omkar139797@gmail.com>
2024-09-29 01:17:52 +0530

    0xE336a92632fA2529F1D2dC5856D0cE713d31e463

--- src/components/ListedBounties.jsx
+++ src/components/ListedBounties.jsx
@@ -1,7 +1,13 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
 import React from 'react';
 import { Card, CardHeader, CardBody } from "@nextui-org/card";
 import { useReadContract } from 'wagmi'; // Make sure you import from the correct library
 import { useAccount } from 'wagmi'
+import { wagmiAbi } from '../abi';
+import { getDefaultConfig } from '@rainbow-me/rainbowkit';
+import { arbitrumGoerli, optimismGoerli, polygonMumbai, sepolia } from 'viem/chains';
+import { readContract } from '@wagmi/core';
 
 // The Card Component for displaying Bounties
 function CardComp({ name, contact, aiAgent, bountyPrize }) {
@@ -18,25 +24,36 @@ function CardComp({ name, contact, aiAgent, bountyPrize }) {
   );
 }
 
-const ListedBounties = () => {
+const ListedBounties = async () => {
 
   const {address} = useAccount()
 
+  const config = getDefaultConfig({
+    appName: 'My RainbowKit App',
+    projectId: 'e7fa7d19fd057ecd9403a0e89bd62b8b',
+    chains: [sepolia, optimismGoerli, arbitrumGoerli, polygonMumbai],
+    ssr: false
+  });
+
   // Destructure data from useReadContract
-  const { data, isLoading, isError } = useReadContract({
-    address: '0x78217d908BD4deD90CE2aE0Bf986447BDFd21B76',
+  const data = await readContract(config,{
+    abi: wagmiAbi,
+    address: '0xE336a92632fA2529F1D2dC5856D0cE713d31e463',
     functionName: 'getForm',
     args: [address],
-    // ...wagmiContractConfig, 
   });
+
+  console.log(data.toString())
+
+
 
 
   // Loading and Error Handling
-  if (isLoading) return <div className="text-white">Loading bounties...</div>;
-  if (isError) return <div className="text-red-500">Error fetching bounties</div>;
+  // if (isLoading) return <div className="text-white">Loading bounties...</div>;
+  // if (isError) return <div className="text-red-500">Error fetching bounties</div>;
 
   // Assuming data has properties for name, contact, aiAgent, and bountyPrize
-  // const { name, contact, aiAgent, bountyPrize } = data || {};
+  const { name, contact, aiAgent, bountyPrize } = data || {};
   
 
   return (
