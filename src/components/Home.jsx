c3c506cc4162bb5977ac4133ad15201ba6879c11
ayushsingh82 <ayushsinghmi711@gmail.com>
2024-09-29 17:23:51 +0530

    ai-agent form

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,56 +1,60 @@
 import React from 'react';
 import { motion } from "framer-motion";
 import { twMerge } from "tailwind-merge";
-
+import { TracingBeam } from "./ui/tracing-beam";
 import { TypewriterEffectSmooth } from './ui/typewriter-effect';
 
+import { BackgroundBeams } from "./ui/background-beams";
+
+function BackgroundBeamsDemo() {
+    return (
+      <div className="h-screen w-screen rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased py-[-200px]">
+        <div className="max-w-2xl mx-auto p-4">
+        <h1 className="relative z-10 text-3xl bg-clip-text  bg-gradient-to-b from-neutral-200 to-neutral-600 text-center font-sans font-bold">
+        <TypewriterEffectSmoothDemo/>
+      </h1>
+      <p></p>
+      <p className="text-neutral-500 max-w-lg mx-auto my-2 text-sm text-center relative z-10">
+        Welcome to AI-Agent bounty, the ultimate AI platform powered by the Gaianet ! 
+        Post bounties, create viral Agents masterpieces, and earn rewards . Whether 
+        you're a ai-agent creator or just a fan, dive into the world of decentralized agents 
+        culture and let your creativity thrive.
+      </p>
+      
+          <button className='mt-[20px] ml-[170px] border-2 border-purple-500 text-white px-[10px] py-[10px] font-medium text-xl bg-gradient-to-r from-purple-500 via-black to-black rounded-lg mx-auto'>
+          Join the community
+      </button>
+        </div>
+        <BackgroundBeams />
+      </div>
+    );
+  }
 
 
-
-const TypewriterEffectSmoothDemo = () => {
+  const TypewriterEffectSmoothDemo = () => {
     const words = [
-        {
-            text: "Run",
-        },
-        {
-            text: "Nodes",
-        },
-        {
-            text: "with",
-        },
-        {
-            text: "Puffer.",
-            className: "text-purple-500 dark:text-purple-500",
-        },
+        { text: "Unleash Your Creativity , create requested " },
+        { text: "AI-Agents , ", className: "text-purple-700" },
+        { text: "&" },
+        { text: "earn " , className: "text-red-700" },
     ];
+
     return (
         <div className="flex flex-col items-center justify-center h-[5rem] mt-[40px]">
             <TypewriterEffectSmooth words={words} />
-            <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
-            </div>
         </div>
     );
-}
+};
 
 function Home() {
     return (
         <div className='flex flex-col items-center bg-black scroller min-h-screen'>
             <div className='text-4xl'>
-                <TypewriterEffectSmoothDemo />
+            <BackgroundBeamsDemo/>
             </div>
-            <h3 className='mt-[20px] text-white text-xl'>
-                Create and manage ad campaigns effortlessly.
-               
-            </h3>
-            <button className='mt-[20px] border-2 border-purple-500 text-white px-[10px] py-[10px] font-medium text-xl bg-gradient-to-r from-purple-900 via-black to-black rounded-lg mx-auto'>
-                Join the community
-            </button>
-            
-               
-            </div>
-     
+           
+        </div>
     )
 }
 
 export default Home
-
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
     return (
       <nav className=''>
         <div className='flex flex-row mx-auto px-[40px] py-[25px] justify-between items-center mt-[0px] bg-black'>
-          <div className='font-bold text-2xl text-purple-400'>
+          <div className='font-semibold text-2xl text-purple-400'>
             <a href='/'>Expanding-Gaia</a>
           </div>
           <div className='flex justify-center flex-1 space-x-8 text-center'>
